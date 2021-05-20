c59413a8e72035909e4cfa676d497cb9f9785f58
Piotrek Antoniak <piotrekantoniakk@gmail.com>
2021-05-20 18:07:42 +0200

    conflicts fixed

--- src/store/types/outfitTypes.ts
+++ src/store/types/outfitTypes.ts
@@ -1,25 +1,24 @@
 import { Cloth } from "./clothTypes";
 
 export interface LikesArray {
-    userId: string;
+  userId: string;
 }
-export interface Outfit{
-    id: string;
-    clothesList: Cloth[];
-    name: string;
-    userId: string;
-    likesCount: number;
-    likes: LikesArray[];
-    calendarDate: string;
+export interface Outfit {
+  id: string;
+  clothesList: Cloth[];
+  name: string;
+  userId: string;
+  likesCount: number;
+  likes: LikesArray[];
+  calendarDate: string;
 }
 export interface MostUsedCloth {
-    cloth: Cloth | null;
-    count: number;
-  }
+  cloth: Cloth | null;
+  count: number;
+}
 
-export interface OutfitState{
-    outfits: Outfit[];
-    mostUsedCloth: MostUsedCloth | null;
-    calendarOutfits: Outfit[];
-
+export interface OutfitState {
+  outfits: Outfit[];
+  mostUsedCloth: MostUsedCloth | null;
+  calendarOutfits: Outfit[];
 }
